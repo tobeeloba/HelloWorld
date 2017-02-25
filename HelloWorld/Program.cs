d8e2e58af6aed570758dba643d5a01b4fd4cf7c8
Ola <popoola.olawunmi9@gmail.com>
2017-02-25 16:20:42 +0100

    Create Hello World Project

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //prints Hello World!
+            Console.WriteLine("Hello World!"); //prints Hello World! to the command window.
+            Console.WriteLine("Input what you want to see on the screen");
+            string input = Console.ReadLine();
+            Console.WriteLine("You typed " + input + ".");
+            Console.ReadLine();  //lets the command window stay on screen without disappearing.
+        }
+    }
+}
